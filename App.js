56b0c93a666e06b41625ece4f646bb0b08c9e56a
jussi_hyde <j.99hyytiainen@gmail.com>
2022-11-27 03:54:01 +0200

    finalizations, ready for publishing

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { API_KEY } from'@env';
 import { NavigationContainer } from'@react-navigation/native';
 import { createBottomTabNavigator } from'@react-navigation/bottom-tabs';
 import { Ionicons} from '@expo/vector-icons';
-import { ListItem } from'react-native-elements';
+import { ListItem, Text } from'react-native-elements';
 import * as SQLite from'expo-sqlite';
 
 const Tab = createBottomTabNavigator();
@@ -15,21 +15,7 @@ const KEY = API_KEY;
 
 const db = SQLite.openDatabase('booksdb.db');
 
-
-const listSeparator = () => {
-  return (
-      <View
-        style={{
-          height: 1,
-          width: "80%",
-          backgroundColor: "#CED0CE",
-          marginLeft: "10%"
-          }}
-        />
-   );
-  };
-
-function HomeScreen( {route} ) {
+function HomeScreen( {  route } ) {
   const {title} = route.params;
   const {author} = route.params;
   const {imageuri} = route.params;
@@ -63,24 +49,27 @@ function HomeScreen( {route} ) {
   }
 
   useEffect(() => {
+    if (title === "") {}
+    else {
     setBooks(title, author, description, imageuri);
     saveItem()
+    };
   }, [title]);
 
   const renderItem = ({ item }) => (
-    <ListItem onLongPress={() => {deleteItem(item.id)}}>
+    <ListItem  onLongPress={() => {deleteItem(item.id)}}>
         <ListItem.Content >
           <ListItem.Title >{item.title}</ListItem.Title>
           <ListItem.Subtitle >{item.author}</ListItem.Subtitle>
           <ListItem.Subtitle >{item.description}</ListItem.Subtitle>
           <Image style={styles.logo} source={{uri: item.imageuri}}/>
+          <ListItem.Subtitle ><Ionicons name="alert-circle-outline" color="red" />long press to delete</ListItem.Subtitle>
         </ListItem.Content>
     </ListItem>
   )
 
   return(
     <View>
-      
       <FlatList
       keyExtractor={(item,index) => index.toString()} 
       renderItem={renderItem}
@@ -93,9 +82,8 @@ function HomeScreen( {route} ) {
 
 }
 
-function SettingsScreen( {navigation} ) {
+function SettingsScreen( {  navigation  } ) {
   const [lists, setLists] = useState([]);
-  const [genre, setGenre] = useState('');
 
   useEffect(() => {
       const getLists = async () => {
@@ -107,13 +95,8 @@ function SettingsScreen( {navigation} ) {
       .catch(console.error);
   }, [])
 
-  useEffect(() => {
-    setGenre(genre)
-  }, [genre])
-  
-
  const renderItem = ({ item }) => (
-    <ListItem bottomDivider onPress={() => {setGenre(item.list_name_encoded); navigation.navigate('Lists', {list: item.list_name_encoded});}}>
+    <ListItem bottomDivider onPress={() => {navigation.navigate('Top Lists', {list: item.list_name_encoded});}}>
       <ListItem.Content>
         <ListItem.Title>{item.list_name}</ListItem.Title>
       </ListItem.Content>
@@ -151,9 +134,6 @@ function ListsScreen({ route, navigation }) {
   const date = getCurrentDate();
   const [repositories, setRepositories] = useState([]);
   
-
-  
-  
   useEffect(() => {
       const getRepositories = async () => {
         const response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/${date}/${list}.json?api-key=${KEY}`)
@@ -168,7 +148,7 @@ function ListsScreen({ route, navigation }) {
     }, [list])
 
   renderItem = ({ item }) => (
-    <ListItem bottomDivider onPress={() => {navigation.navigate('Home', {title: item.title, author: item.author, description: item.description, imageuri: item.book_image });}}>
+    <ListItem bottomDivider onPress={() => {navigation.navigate('My books', {title: item.title, author: item.author, description: item.description, imageuri: item.book_image });}}>
         <ListItem.Content>
           <ListItem.Title>{item.rank}. {item.title}</ListItem.Title>
           <ListItem.Subtitle >{item.author}</ListItem.Subtitle>
@@ -178,10 +158,10 @@ function ListsScreen({ route, navigation }) {
       </ListItem>
     )
 
-
   return (
     <View >
       <StatusBar hidden={true} />
+      <Text h4 >{list.charAt(0).toUpperCase() + list.slice(1)}</Text>
       <FlatList
         keyExtractor={(item,index) => index.toString()}
         renderItem={renderItem}
@@ -197,24 +177,37 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator screenOptions={screenOptions}>
-        <Tab.Screen name="Home" component={HomeScreen} initialParams={{title: ""}}/>
+        <Tab.Screen name="My books" component={HomeScreen} initialParams={{title: ""}}/>
         <Tab.Screen name="Change Genre" component={SettingsScreen} />
-        <Tab.Screen name="Lists" component={ListsScreen} initialParams={{list: "hardcover-fiction"}}/>
+        <Tab.Screen name="Top Lists" component={ListsScreen} initialParams={{list: "hardcover-fiction"}}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
+const listSeparator = () => {
+  return (
+      <View
+        style={{
+          height: 1,
+          width: "80%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "10%"
+          }}
+        />
+   );
+  };
+
 const screenOptions = ({ route }) => ({
-  tabBarIcon: ({ focused, color, size }) => {
+  tabBarIcon: ({ color, size }) => {
     let iconName;
 
-    if (route.name === 'Home') {
-      iconName = 'md-home';
+    if (route.name === 'My books') {
+      iconName = 'md-book-outline';
     } else if (route.name === 'Change Genre') {
-      iconName = 'md-settings';
-    } else if (route.name === 'Lists') {
-      iconName = 'md-list';
+      iconName = 'build-outline';
+    } else if (route.name === 'Top Lists') {
+      iconName = 'clipboard-outline';
     }
 
     return <Ionicons name={iconName} size={size} color={color} />;
